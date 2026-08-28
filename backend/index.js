fd737ab01b9e65a4087ae928b16ff71d9096ebfc
suraj patil <er.surajpatil22@gmail.com>
2026-08-28 17:22:20 +0530

    Fix CORS for localhost and production

--- backend/index.js
+++ backend/index.js
@@ -24,15 +24,25 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://course-selling-app-frontend-yg7b.onrender.com",
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 const port = process.env.PORT || 5000;
 const DB_URI = process.env.MONGO_URI;
 
